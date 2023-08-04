f5b25c098be81df966b907d397376c4667289d1d
lemuel.lima <lemuel.lima@dwrpa.com.br>
2023-08-04 00:09:00 -0300

    Adicionar arquivos de projeto.

---
+++ ApiBlogBalta/Data/BlogDataContext.cs
@@ -0,0 +1,24 @@
+using ApiBlogBalta.Data.Mappings;
+using ApiBlogBalta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiBlogBalta.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public BlogDataContext(DbContextOptions<BlogDataContext> options) : base(options)
+        {
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new PostMap());
+        }
+    }
+}
---
+++ ApiBlogBalta/Services/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using ApiBlogBalta.Extensions;
+using ApiBlogBalta.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiBlogBalta.Services
+{
+    public class TokenService
+    {
+        public string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            var claims = user.GetClaims();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
